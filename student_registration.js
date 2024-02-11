c1c5e8326de28092f9657b496bdf7210e8e4b981
Lazytech15 <149139327+Lazytech15@users.noreply.github.com>
2024-02-11 13:30:37 +0800

    student_registration.js

--- student_registration.js
+++ student_registration.js
@@ -1,14 +1,14 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-app.js";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDFWV4k9-XmABgNGmleyXLTcuEn41rMHK8",
-    authDomain: "hackathon-26f12.firebaseapp.com",
-    databaseURL: "https://hackathon-26f12-default-rtdb.firebaseio.com",
-    projectId: "hackathon-26f12",
-    storageBucket: "hackathon-26f12.appspot.com",
-    messagingSenderId: "1071789540560",
-    appId: "1:1071789540560:web:6227da20f3a3a3a9ab0ad5"
-  };
+  apiKey: "AIzaSyByH0pNuEoNXna4Dj61C2QxIX-AfmFAnq0",
+  authDomain: "antipolo-hackathon-project.firebaseapp.com",
+  projectId: "antipolo-hackathon-project",
+  storageBucket: "antipolo-hackathon-project.appspot.com",
+  messagingSenderId: "88056856756",
+  appId: "1:88056856756:web:9597da80bb7239996bd7e1"
+};
 
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
